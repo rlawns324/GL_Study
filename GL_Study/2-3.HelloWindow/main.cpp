bc023e1e9851f6c658eb13f4cb3ce19fdc7f2a15
JuneKim <june324@snowcorp.com>
2020-10-05 10:04:57 +0900

    2_3.HelloWindow"

--- GL_Study/2-3.HelloWindow/main.cpp
+++ GL_Study/2-3.HelloWindow/main.cpp
@@ -7,18 +7,11 @@
 // GLFW
 #include <GLFW/glfw3.h>
 
-extern GLuint LoadShaders(const char* vsource, const char* fsource);
 const GLuint WIDTH = 800, HEIGHT = 600;
 void processInput(GLFWwindow *window);
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 int main()
 {
-    std::cout <<"Shader MACRO Test"<<std::endl;
-    std::cout<< V_SHADER <<std::endl;
-    //${PROJECT_DIR}/GL_Study/shader.vert\
-    //Users/user/snow/GL_Study/GL_Study/shader.vert
-    std::cout<< F_SHADER <<std::endl;
-    
     // Init GLFW
     glfwInit( );
     
@@ -27,11 +20,10 @@ int main()
     glfwWindowHint( GLFW_CONTEXT_VERSION_MINOR, 3 );
     glfwWindowHint( GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE );
     glfwWindowHint( GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE );
-
     glfwWindowHint( GLFW_RESIZABLE, GL_TRUE );
     
     // Create a GLFWwindow object that we can use for GLFW's functions
-    GLFWwindow *window = glfwCreateWindow( WIDTH, HEIGHT, "LearnOpenGL", nullptr, nullptr );
+    GLFWwindow *window = glfwCreateWindow( WIDTH, HEIGHT, "Lecture 2_3.HelloWindow", nullptr, nullptr );
     
     int screenWidth, screenHeight;
     glfwGetFramebufferSize( window, &screenWidth, &screenHeight );
@@ -58,33 +50,6 @@ int main()
     // Define the viewport dimensions
     glViewport(0, 0, screenWidth, screenHeight );
     
-    char * vs = "/Users/user/snow/GL_Study/GL_Study/shader.vert";
-    GLuint shaderProgram = LoadShaders(vs, F_SHADER);
-    glUseProgram(shaderProgram);
-    
-    // Set up vertex data (and buffer(s)) and attribute pointers
-    GLfloat vertices[] =
-    {
-        -0.5f, -0.5f, 0.0f, // Left
-        0.5f, -0.5f, 0.0f, // Right
-        0.0f,  0.5f, 0.0f  // Top
-    };
-    
-    GLuint VBO, VAO;
-    glGenVertexArrays( 1, &VAO );
-    glGenBuffers( 1, &VBO );
-    // Bind the Vertex Array Object first, then bind and set vertex buffer(s) and attribute pointer(s).
-    glBindVertexArray( VAO );
-    
-    glBindBuffer( GL_ARRAY_BUFFER, VBO );
-    glBufferData( GL_ARRAY_BUFFER, sizeof( vertices ), vertices, GL_STATIC_DRAW );
-    
-    glVertexAttribPointer( 0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof( GLfloat ), ( GLvoid * ) 0 );
-    glEnableVertexAttribArray( 0 );
-    
-    glBindBuffer( GL_ARRAY_BUFFER, 0 ); // Note that this is allowed, the call to glVertexAttribPointer registered VBO as the currently bound vertex buffer object so afterwards we can safely unbind
-    
-    glBindVertexArray( 0 ); // Unbind VAO (it's always a good thing to unbind any buffer/array to prevent strange bugs)
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
     // Game loop
     while (!glfwWindowShouldClose( window ))
@@ -98,20 +63,10 @@ int main()
         glClearColor( 0.2f, 0.3f, 0.3f, 1.0f );
         glClear( GL_COLOR_BUFFER_BIT );
         
-        // Draw our first triangle
-        glUseProgram( shaderProgram );
-        glBindVertexArray( VAO );
-        glDrawArrays( GL_TRIANGLES, 0, 3 );
-        glBindVertexArray( 0 );
-        
         // Swap the screen buffers
         glfwSwapBuffers( window );
     }
-    
-    // Properly de-allocate all resources once they've outlived their purpose
-    glDeleteVertexArrays( 1, &VAO );
-    glDeleteBuffers( 1, &VBO );
-    
+        
     // Terminate GLFW, clearing any resources allocated by GLFW.
     glfwTerminate( );
     
@@ -128,3 +83,4 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
     glViewport(0, 0, width, height);
 }
+
